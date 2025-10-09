6f1485df4197d55395180282582278c5df22bc72
DevJaredt <jaredtrujilloespinosa64@gmail.com>
2025-10-09 02:00:09 -0500

    Define ITodo interface and implement todoSchema using mongoose for Todo model

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,7 +1,18 @@
-export interface Todo {
+import mongoose from "mongoose";
+
+export interface ITodo extends Document {
   id: string;
   title: string;
   description: string;
   completed: boolean;
   createdAt: Date;
 }
+
+const todoSchema = new mongoose.Schema<ITodo>({
+  title: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const todoModel = mongoose.model<ITodo>("Todo", todoSchema);
